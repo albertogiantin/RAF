eac9a31914dd6104bcab0effde1c255a75e98d44
Alberto Giantin <albertogiantin@gmail.com>
2014-08-13 16:41:18 +0200

    Chart

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,9 +3,75 @@
 /* Controllers */
 
 angular.module('myApp.controllers', [])
-  .controller('AdeguatezzaComplessivaCtrl', ['$scope', function($scope) {
+	.controller('AdeguatezzaComplessivaCtrl', ['$scope',
+		function($scope) {
+			var chart1 = {};
+			chart1.type = "PieChart";
+			chart1.data = {
+				"cols": [{
+					id: "obiettivo",
+					label: "Obiettivo",
+					type: "string"
+				}, {
+					id: "valore",
+					label: "Valore",
+					type: "number"
+				}],
+				"rows": [{
+					c: [{
+						v: "Business"
+					}, {
+						v: 35
+					}]
+				}, {
+					c: [{
+						v: "Adeguatezza Patrimoniale"
+					}, {
+						v: 22
+					}]
+				}, {
+					c: [{
+						v: "Redditività"
+					}, {
+						v: 5
+					}]
+				}, {
+					c: [{
+						v: "Liquidità / Struttura finanziaria"
+					}, {
+						v: 12
+					}]
+				}, {
+					c: [{
+						v: "Rischiosità"
+					}, {
+						v: 12
+					}]
+				}]
+			};
 
-  }])
-  .controller('MyCtrl2', ['$scope', function($scope) {
+			chart1.options = {
+				"isStacked": "true",
+				"fill": 20,
+				"displayExactValues": true,
+				"vAxis": {
+					"title": "Sales unit",
+					"gridlines": {
+						"count": 6
+					}
+				},
+				"hAxis": {
+					"title": "Date"
+				}
+			};
 
-  }]);
+			chart1.formatters = {};
+
+			$scope.chart = chart1;
+		}
+	])
+	.controller('MyCtrl2', ['$scope',
+		function($scope) {
+
+		}
+	]);
