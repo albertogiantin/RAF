c4097232585da477fa0b653e30e8078550a8d116
Alberto Giantin <albertogiantin@gmail.com>
2014-08-13 15:44:38 +0200

    no message

--- app/js/app.js
+++ app/js/app.js
@@ -10,7 +10,10 @@ angular.module('myApp', [
   'myApp.controllers'
 ]).
 config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
+  $routeProvider.when('/adeguatezzaComplessivaView', 
+  	{templateUrl: 'partials/adeguatezzaComplessiva.html', 
+  	controller: 'AdeguatezzaComplessivaCtrl'}
+  );
   $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
-  $routeProvider.otherwise({redirectTo: '/view1'});
+  $routeProvider.otherwise({redirectTo: '/adeguatezzaComplessivaView'});
 }]);
